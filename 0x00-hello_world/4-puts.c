26b0004e4f4fc73b3523a0cfd817492f28a41609
rakel123 <qnrakel3@gmail.com>
2023-06-16 01:03:31 -0700

    Writing my first string in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A file to print a word
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
