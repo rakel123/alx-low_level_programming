e5aaa3927df3ace2d2093463038ed4f8e8a93cda
rakel123 <qnrakel3@gmail.com>
2023-06-19 19:16:50 -0700

    printing both capital letters and lowercase using the putchar funtion

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: Program to print eh letters of the alphabet in lower case
+* Return: always (0) success
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 97; i <= 122; i++)
+{
+putchar(i);
+
+}
+for (i = 65; i <= 90; i++)
+{
+putchar(i);
+putchar('\n');
+}
+return (0);
+}
