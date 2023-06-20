3de0a0760eb1ecd3da65f47f19d85d978406c1bb
rakel123 <qnrakel3@gmail.com>
2023-06-19 18:41:20 -0700

    Displaying letters of the alphabet in lowercase letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: Program to print eh letters of the alphabet in lower case
+* Return: always (0) success
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 97; i <= 122; i++)
+{
+	putchar(i);
+}
+return (0);
+}
