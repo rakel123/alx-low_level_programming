62e25fc13ef03fb2649f3d7f13a0a861440be5e8
rakel123 <qnrakel3@gmail.com>
2023-06-21 15:25:08 -0700

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - displays time format of hh/mm foe 24 hours
+ * hour: gives the hour to be displayed
+ * minute: minutes to be displayed
+ *Return: nothing
+ */
+
+void jack_bauer(void)
+{
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar('0' + hour / 10);
+			_putchar('0' + hour % 10);
+			_putchar(':');
+			_putchar('0' + minute / 10);
+			_putchar('0' + minute % 10);
+			_putchar('\n');
+		}
+	}
+}
