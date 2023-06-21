322579d398bcc8122f3680018674f1aa8884c1f6
rakel123 <qnrakel3@gmail.com>
2023-06-21 11:21:56 -0700

    testing letters of alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @c - tests a character
+ * @c: character to be checked
  * Return; 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
 
