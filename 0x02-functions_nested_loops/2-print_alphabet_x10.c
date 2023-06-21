ad2e919360df4b61bd628ab448daa4024be8e435
rakel123 <qnrakel3@gmail.com>
2023-06-21 03:27:40 -0700

    lower case alphabet in 10 lines

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,18 @@
 void print_alphabet_x10(void)
 {
 int i;
+char c;
 
-for (i = 0; i <= 10; i++)
+for (i = 0; i < 10; i++)
 {
 
-for (i = 97; i <= 122; i++)
+for (c = 'a'; c <= 'z'; c++)
 {
-_putchar(i);
-}
-
+_putchar(c);
 }
 
 _putchar('\n');
+
+}
+
 }
